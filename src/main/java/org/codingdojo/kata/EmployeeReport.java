d481460e4295e66ec28936d50742cfa001e9741e
Rachid OUHSSAINE <rachid.ouhssaine@outlook.com>
2022-08-21 20:11:28 +0200

    [EmployeeReport] Second step : GREEN

--- src/main/java/org/codingdojo/kata/EmployeeReport.java
+++ src/main/java/org/codingdojo/kata/EmployeeReport.java
@@ -1,5 +1,6 @@
 package org.codingdojo.kata;
 
+import java.util.Comparator;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -15,6 +16,7 @@ public class EmployeeReport {
     public List<Employee> getAdult() {
         return employees.stream()
                 .filter(Employee::isAdult)
+                .sorted(Comparator.comparing(Employee::name))
                 .collect(toList());
     }
 }
